c170eae57b94b19ebde3055d98f5f1798c442d40
DuyNH <hoangduy791999@gmail.com>
2021-10-22 15:50:25 +0700

    add login-modal.html, signup-modal.html, modal.css, scroll-button.js

---
+++ js/scroll-button.js
@@ -0,0 +1,12 @@
+var mybutton = document.getElementById("scroll-button");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
