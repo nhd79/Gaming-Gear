2f4840bb321e9052a8dc9e8e0cc412e242070308
DuyNH <hoangduy791999@gmail.com>
2021-10-30 18:29:54 +0700

    prepare sql footer

--- controller/footer-brand.php
+++ controller/footer-brand.php
@@ -1,6 +1,6 @@
 <?php
 $sql = 'select * from brand';
-$result = readDatabase($sql);
+$result = readDatabase($sql, array());
 foreach ($result as $brand) {
     echo '<li><a href="' . $brand[1] . '" target="_blank">' . $brand[0] . '</a></li>';
 }
--- controller/footer-product.php
+++ controller/footer-product.php
@@ -1,6 +1,6 @@
 <?php
 $sql = 'select * from category';
-$result = readDatabase($sql);
+$result = readDatabase($sql, array());
 foreach ($result as $category) {
     echo '<li><a href="product.php?category_id=' . $category[0] . '">' . $category[1] . '</a></li>';
 }
