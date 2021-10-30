43b5f8b8d11419a6081ff650b5372e99134fd556
DuyNH <hoangduy791999@gmail.com>
2021-10-30 15:11:24 +0700

    done login password verify

--- controller/login.php
+++ controller/login.php
@@ -6,19 +6,25 @@ include_once("form-validation.php");
 if (isset($_POST["username"])) {
     $username = test_input($_POST["username"]);
     $password = test_input($_POST["password"]);
-    $sql = "select * from customer where user_name='" . $username . "' and password='" . $password . "'";
+
+    $sql = "select * from customer where user_name='" . $username . "'";
     $result = readDatabase($sql);
     if ($result->rowCount() > 0) {
         foreach ($result as $customer) {
-            $_SESSION["full_name"] = $customer["full_name"];
-            $_SESSION["id"] = $customer["id"];
-            $_SESSION["email"] = $customer["email"];
-            $_SESSION["phone"] = $customer["phone"];
-            $_SESSION["address"] = $customer["address"];
-            $_SESSION["image"] = $customer["image"];
+            if (password_verify($password, $customer["password"])) {
+                $_SESSION["full_name"] = $customer["full_name"];
+                $_SESSION["id"] = $customer["id"];
+                $_SESSION["email"] = $customer["email"];
+                $_SESSION["phone"] = $customer["phone"];
+                $_SESSION["address"] = $customer["address"];
+                $_SESSION["image"] = $customer["image"];
+            } else {
+                echo "<script>alert('Sai mật khẩu!');</script>";
+            }
         }
+        echo "<script>alert('Đăng nhập thành công!');</script>";
         echo "<script>location.href = 'index.php';</script>";
     } else {
-        echo "<script>alert('Sai thông tin tài khoản hoặc mật khẩu!');</script>";
+        echo "<script>alert('Sai thông tin tài khoản!');</script>";
     }
 }
