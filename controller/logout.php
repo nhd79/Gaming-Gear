06362cd6d01df5461ef41ce3b6b9cca1b8a3c6e9
DuyNH <hoangduy791999@gmail.com>
2021-11-21 09:36:37 +0700

    color things

--- controller/logout.php
+++ controller/logout.php
@@ -1,7 +1,8 @@
 <?php
 $name = $_SESSION["full_name"];
 if ($name == "admin") {
-    header('Location: /view/admin-dashboard.html');
+    // echo "<script>location.href = 'admin-dashboard.php';</script>";
+    header('Location: admin-dashboard.php');
 } else {
     echo '<a href="user.php">KHÁCH HÀNG</a>
     <a href="index.php?logout=1">ĐĂNG XUẤT</a>
