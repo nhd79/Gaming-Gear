8c5bf42ca8267d06b79fec88b2a3e6f8ebd8655f
{Duy} <hoangduy791999@gmail.com>
2021-11-24 21:06:43 +0700

    working on product page

--- view/dashboard-product.php
+++ view/dashboard-product.php
@@ -21,95 +21,49 @@
       include_once("dashboard-topbar.php");
       ?>
 
-      <div class="details">
-        <!-- order details list -->
-        <div class="recentOrders">
-          <div class="cardHeader">
-            <h2>Đơn hàng gần đây</h2>
-            <a href="#" class="btn">Xem tất cả</a>
+      <div class="product-list">
+        <div class="product">
+          <div>
+            <div class="">Image</div>
           </div>
-          <table>
-            <thead>
-              <tr>
-                <td>Tên</td>
-                <td>Giá</td>
-                <td>Hình thức thanh toán</td>
-                <td>Trạng thái</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Star Refrigerator</td>
-                <td>$1200</td>
-                <td>Online Banking</td>
-                <td><span class="status delivered">Đã giao hàng</span></td>
-              </tr>
-              <tr>
-                <td>Window Coolers</td>
-                <td>$110</td>
-                <td>COD</td>
-                <td><span class="status pending">Đang xử lý</span></td>
-              </tr>
-              <tr>
-                <td>Speakers</td>
-                <td>$620</td>
-                <td>Online Banking</td>
-                <td><span class="status return">Trả hàng</span></td>
-              </tr>
-              <tr>
-                <td>HP Laptop</td>
-                <td>$110</td>
-                <td>COD</td>
-                <td><span class="status inprogress">Đang giao hàng</span></td>
-              </tr>
-              <tr>
-                <td>Apple Watch</td>
-                <td>$1200</td>
-                <td>Online Banking</td>
-                <td><span class="status delivered">Đã giao hàng</span></td>
-              </tr>
-              <tr>
-                <td>Wall Fan</td>
-                <td>$110</td>
-                <td>Online Banking</td>
-                <td><span class="status pending">Đang xử lý</span></td>
-              </tr>
-              <tr>
-                <td>Adidas Shoes</td>
-                <td>$620</td>
-                <td>Online Banking</td>
-                <td><span class="status return">Trả hàng</span></td>
-              </tr>
-              <tr>
-                <td>Denim Shirts</td>
-                <td>$110</td>
-                <td>COD</td>
-                <td><span class="status inprogress">Đang giao hàng</span></td>
-              </tr>
-              <tr>
-                <td>Casual Shoes</td>
-                <td>$575</td>
-                <td>Online Banking</td>
-                <td><span class="status pending">Đang xử lý</span></td>
-              </tr>
-              <tr>
-                <td>Wall Fan</td>
-                <td>$110</td>
-                <td>Online Banking</td>
-                <td><span class="status pending">Đang xử lý</span></td>
-              </tr>
-              <tr>
-                <td>Denim Shirts</td>
-                <td>$110</td>
-                <td>COD</td>
-                <td><span class="status inprogress">Đang giao hàng</span></td>
-              </tr>
-            </tbody>
-          </table>
+          <div>
+            <div class="">Bàn phím</div>
+          </div>
+          <div>
+            <div class="">Bàn phím Rapoo V500L Rainbow</div>
+          </div>
+          <div>
+            <div class="">490000 VND</div>
+          </div>
+          <div>
+            <div class="">Description</div>
+          </div>
         </div>
-
       </div>
+      <div class="product-list">
+        <div class="product">
+          <div>
+            <div class="">Image</div>
+          </div>
+          <div>
+            <div class="">Bàn phím</div>
+          </div>
+          <div>
+            <div class="">Bàn phím Rapoo V500L Rainbow</div>
+          </div>
+          <div>
+            <div class="">490000 VND</div>
+          </div>
+          <div>
+            <div class="">Description</div>
+          </div>
+        </div>
+      </div>
+
+
+
     </div>
+  </div>
   </div>
   </div>
 
