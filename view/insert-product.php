b9bd96a8c3b987242a5d18c422d3bbd68b324208
{Duy} <hoangduy791999@gmail.com>
2021-11-26 20:33:28 +0700

    done insert-product front-end

--- view/insert-product.php
+++ view/insert-product.php
@@ -6,6 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/css/dashboard-styles.css" />
+
+    <!-- Remember to include jQuery :) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+    <!-- jQuery Modal -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+
     <title>Admin Dashboard</title>
 </head>
 
@@ -21,7 +29,7 @@
             include_once("dashboard-topbar.php");
             ?>
 
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+            <form method="post" action="">
                 <div class="insert-product-grid">
                     <div class="header">
                         <h2>Thêm sản phẩm</h2>
@@ -30,52 +38,77 @@
                         Tên sản phẩm:
                     </div>
                     <div>
-                        <input type="text" name="" />
+                        <input type="text" name="name" id="name" required />
                     </div>
                     <div>
                         Loại sản phẩm:
                     </div>
                     <div>
-                        <input type="text" name="" />
+                        <select name="category">
+                            <option value="1">Bàn phím</option>
+                            <option value="2">Chuột</option>
+                            <option value="3">Tai nghe</option>
+                            <option value="4">Ghế</option>
+                            <option value="5">Loa</option>
+                            <option value="6">Lót chuột</option>
+                        </select>
                     </div>
                     <div>
                         Đơn giá:
                     </div>
                     <div>
-                        <input type="text" name="" />
+                        <input type="number" name="price" id="price" required />
                     </div>
                     <div>
                         Mô tả:
                     </div>
                     <div>
-                        <input type="text" name="" />
+                        <input type="text" name="description" id="description" required />
                     </div>
                     <div>
                         Mô tả 2:
                     </div>
                     <div>
-                        <input type="text" name="" />
+                        <input type="text" name="description_2" id="description_2" />
                     </div>
                     <div>
                         Mô tả 3:
                     </div>
                     <div>
-                        <input type="text" name="" />
+                        <input type="text" name="description_3" id="description_3" />
                     </div>
                     <div>
                         Mô tả 4:
                     </div>
                     <div>
-                        <input type="text" name="" />
+                        <input type="text" name="description_4" id="description_4" />
                     </div>
                     <div>
                         Mô tả 5:
                     </div>
                     <div>
-                        <input type="text" name="" />
+                        <input type="text" name="description_5" id="description_5" />
                     </div>
-                    <div class="product-img">IMG</div>
-                    <div class="submit">Submit Button</div>
+                    <!-- <div class="product-img">IMG</div> -->
+                    <div class="product-img">
+                        <label class="img-label">
+                            <input type="file" name="file" class="img-input" required />
+                            <span class="text">Chọn ảnh sản phẩm&ensp;</span></label>
+                        <script>
+                            //Name appear
+                            $(".img-input").on("change", function() {
+                                var fileName = $(this).val().split("\\").pop();
+                                $(this)
+                                    .siblings(".text")
+                                    .html(fileName + "&ensp;");
+                            });
+                        </script>
+                    </div>
+                    <div class="submit">
+                        <button type="submit" class="order-button">
+                            Thêm sản phẩm mới
+                        </button>
+                    </div>
                 </div>
             </form>
 
