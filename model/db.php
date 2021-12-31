a181a22dfcf9c44576238b886c3e5b1a5fff5b49
DuyNH <hoangduy791999@gmail.com>
2021-12-31 15:31:08 +0700

    disable ssl

--- model/db.php
+++ model/db.php
@@ -20,7 +20,7 @@ function readDatabase($sql, $parameter)
     $options = array(
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::MYSQL_ATTR_SSL_CA => '\CA-Cert\DigiCertGlobalRootCA.crt.pem'
+        // PDO::MYSQL_ATTR_SSL_CA => '\CA-Cert\DigiCertGlobalRootCA.crt.pem'
     );
     try {
         $connection = new PDO('mysql:host=' . $servername . ';dbname=' . $dbname . '', $dbusername, $dbpassword, $options);
