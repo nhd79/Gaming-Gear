1382b91285964e4ce0dea2d54fe480b60c9193af
{Duy} <hoangduy791999@gmail.com>
2021-12-31 20:18:20 +0700

    move db connection out of function

--- model/db.php
+++ model/db.php
@@ -9,54 +9,34 @@ $dbname = "grid-gaming-gear";
 // $dbpassword = "";
 // $dbname = "grid-gaming-gear";
 
-// $servername = "fdb34.awardspace.net";
-// $dbusername = "4015136_gridgg";
-// $dbpassword = "3T[lQAHo0sTX7?B_";
-// $dbname = "4015136_gridgg";
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    // PDO::MYSQL_ATTR_SSL_CA => '\CA-Cert\DigiCertGlobalRootCA.crt.pem'
+);
+try {
+    $connection = new PDO('mysql:host=' . $servername . ';dbname=' . $dbname . '', $dbusername, $dbpassword, $options);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 function readDatabase($sql, $parameter)
 {
-    global $servername, $dbusername, $dbpassword, $dbname;
-    $options = array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        // PDO::MYSQL_ATTR_SSL_CA => '\CA-Cert\DigiCertGlobalRootCA.crt.pem'
-    );
-    try {
-        $connection = new PDO('mysql:host=' . $servername . ';dbname=' . $dbname . '', $dbusername, $dbpassword, $options);
-        // echo "Connected successfully";
-        $prepare_sql = $connection->prepare($sql);
-        $prepare_sql->execute($parameter);
-        $result = $prepare_sql->fetchAll();
-        // $result = $connection->query($sql);
-        return $result;
-        $connection = null;
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
+    global $connection;
+    $prepare_sql = $connection->prepare($sql);
+    $prepare_sql->execute($parameter);
+    $result = $prepare_sql->fetchAll();
+    return $result;
 }
-// $result = readDatabase('select * from category where 1');
-// foreach ($result as $danhmuc) {
-//     echo $danhmuc[0];
-//     echo $danhmuc[1];
-// }
 
 function writeDatabase($sql, $parameter)
 {
-    global $servername, $dbusername, $dbpassword, $dbname;
-    $options = array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    );
-
-    $connection = new PDO('mysql:host=' . $servername . ';dbname=' . $dbname . '', $dbusername, $dbpassword, $options);
-
+    global $connection;
     try {
         $connection->beginTransaction();
 
         $prepare_sql = $connection->prepare($sql);
         $prepare_sql->execute($parameter);
-        // $connection->exec($sql);
 
         $connection->commit();
     } catch (PDOException $e) {
